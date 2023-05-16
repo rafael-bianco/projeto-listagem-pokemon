7bf4cf4337c2e6a1e7ae4b234fbab75acee9bd1a
rafaelbianco19 <49214028+rafaelbianco19@users.noreply.github.com>
2023-05-15 21:40:33 -0300

    index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 const botaoAlterarTema = document.getElementById("botao-alterar-tema");
 const body = document.querySelector("body");
 const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+const imagemPokebola = document.querySelector(".logo")
+
 
 botaoAlterarTema.addEventListener("click", () => {
     const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
@@ -9,12 +11,12 @@ botaoAlterarTema.addEventListener("click", () => {
 
     if (modoEscuroEstaAtivo) {
         imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+        imagemPokebola.setAttribute("src", "./src/imagens/pokeball.png");
     } else {
         imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
+        imagemPokebola.setAttribute("src", "./src/imagens/ultraball.png");
     }
 });
-
-
 
 
 
